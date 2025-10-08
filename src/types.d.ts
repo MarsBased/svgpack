e9624c5aa15545cdc666a9f8c0d8b6cc2fd714a4
Javier Artero <javier.artero@gmail.com>
2025-10-08 12:22:13 +0200

    fix: declare module para el subpath

--- src/types.d.ts
+++ src/types.d.ts
@@ -13,6 +13,18 @@ export interface SvgpackMaskProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 declare module '@marsbased/svgpack/components' {
+  export interface SvgpackBackgroundProps extends React.HTMLAttributes<HTMLDivElement> {
+    image?: string;
+    width?: string;
+    height?: string;
+  }
+
+  export interface SvgpackMaskProps extends React.HTMLAttributes<HTMLDivElement> {
+    image?: string;
+    width?: string;
+    height?: string;
+  }
+
   export function SvgpackBackground(props: SvgpackBackgroundProps): JSX.Element;
   export function SvgpackMask(props: SvgpackMaskProps): JSX.Element;
 }
