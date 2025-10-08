ccfa6c08f837aebf35ad82063af4c05a1bdbb235
Javier Artero <javier.artero@gmail.com>
2025-10-08 12:27:26 +0200

    fix: export type from './types'

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 // Export React components
 export { SvgpackMask, SvgpackBackground } from './components/index';
-export type { SvgpackBackgroundProps, SvgpackMaskProps } from './types.d.ts';
+export type { SvgpackBackgroundProps, SvgpackMaskProps } from './types';
