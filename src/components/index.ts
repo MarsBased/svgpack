1c85513b386ddf7191e3b0a8228dac3e57adc18f
Javier Artero <javier.artero@gmail.com>
2025-10-08 15:02:23 +0200

    fix: streamline component exports by removing redundant type exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,2 @@
 export { SvgpackMask } from './SvgpackMask.js';
 export { SvgpackBackground } from './SvgpackBackground.js';
-export type { SvgpackMaskProps } from './SvgpackMask.d.ts';
-export type { SvgpackBackgroundProps } from './SvgpackBackground.d.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 // Export React components
 export { SvgpackMask, SvgpackBackground } from './components/index';
-export type { SvgpackMaskProps, SvgpackBackgroundProps } from './components/index';
